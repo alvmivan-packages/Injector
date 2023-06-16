fd4a20323340f7cad83b3a573cec2a9e1ecebe11
Marcos I. Alvarez <malvarez@gametaco.com>
2023-06-16 07:48:32 -0300

    all

---
+++ Runtime/Core/DependenciesInjector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Injector
+{
+    public interface IDependenciesInjector
+    {
+        T Get<T>() where T : class;
+        void Register<TInterface, TImplementation>() where TImplementation : class, TInterface;
+        void Register<T>(T instance) where T : class;
+        void RegisterRawType(Type type, object instance);
+        void Register<TClass>() where TClass : class;
+        void Clear<T>() where T : class;
+    }
+}
---
+++ Runtime/TypesTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Injector
+{
+    public class TypesTool
+    {
+        public static Type[] ClassesBetween(Type bottomClass, Type topClass)
+        {
+            if (!topClass.IsClass) throw new Exception($"{topClass} is not a class");
+            if (!bottomClass.IsClass) throw new Exception($"{bottomClass} is not a class");
+
+            var types = new List<Type>();
+            var currentType = bottomClass;
+
+            while (currentType != topClass && currentType != null)
+            {
+                types.Add(currentType);
+                currentType = currentType.BaseType;
+            }
+
+            if (currentType == null || currentType != topClass)
+            {
+                throw new Exception($"Can't find {topClass} in inheritance chain of {bottomClass}");
+            }
+
+            if (currentType == topClass)
+            {
+                return types.ToArray();
+            }
+
+            throw new Exception($"Unknown error in {nameof(ClassesBetween)}");
+        }
+    }
+}
